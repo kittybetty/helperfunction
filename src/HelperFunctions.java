6a3bf2c400b3d192b00809de319939fc02dbd7a1
Jing W <jenny.wen1994@gmail.com>
2017-10-19 22:22:56 -0400

    change pagination

--- src/HelperFunctions.java
+++ src/HelperFunctions.java
@@ -1,36 +1,22 @@
 /**
- * Todo: Time based filtering queries; first, prev, self, next, last
+ * Todo: Time based filtering queries; first, prev, self, next, last; stringbuilder; skip character; invalid;
  * 
  * test
  */
 public class HelperFunctions {
 
-    // Assume marker is ID
-    // GET /app/items?limit=30
-    public static String getFirstPage(int limit) {
-        String sql = "SELECT * FROM [database] LIMIT " + limit;
-//       int marker = "SELECT ID FROM (SELECT * FROM [database] LIMIT " + limit + ") ORDER BY ID DESC";
-        return sql;
-    }
-
-    // GET /app/items?limit=30&marker=08ec231f6d9a43dda97d4b950c3393df
-    public static String getPage(int limit, String marker) {
-        String sql = "SELECT * FROM [database] WHERE ID > " + marker + " LIMIT " + limit;
-        return sql;
-    }
-
-    public static String paginationDriver(String input) {
+    // GET /app/items?limit=30&page=2
+    public static String getPage(String input) {
         int limitIndex = input.indexOf("limit=");
-        String limitString = input.substring(limitIndex + 6);
-        int markerIndex = input.indexOf("marker=");
-        if (markerIndex < 0) {
-            int limit = Integer.parseInt(limitString);
-            return getFirstPage(limit);
-        } else {
-            limitString = input.substring(limitIndex + 6, markerIndex - 1);
-            String marker = input.substring(markerIndex + 7);
-            return getPage(Integer.parseInt(limitString), marker);
-        }
+        int pageIndex = input.indexOf("page=");
+
+        String limitString = input.substring(limitIndex + 6, pageIndex - 1);
+        String pageString = input.substring(pageIndex + 5);
+        int limit = Integer.parseInt(limitString);
+        int page = Integer.parseInt(pageString);
+        int offset = (page - 1 ) * limit;
+        String sql = "SELECT * FROM [database] LIMIT " + limit + " OFFSET " +  offset;
+        return sql;
     }
 
     // /app/items?foo=buzz&baz=quux
@@ -97,8 +83,7 @@ public class HelperFunctions {
     }
 
     public static void main(String[] args) {
-        System.out.println(paginationDriver("GET /app/items?limit=30"));
-        System.out.println(paginationDriver("GET /app/items?limit=30&marker=08ec231f6d9a43dda97d4b950c3393df"));
+        System.out.println(getPage("GET /app/items?limit=30&page=2"));
 
         System.out.println(filterEqual("/app/items?foo=buzz"));
         System.out.println(filterEqual("/app/items?foo=buzz&baz=quux"));
